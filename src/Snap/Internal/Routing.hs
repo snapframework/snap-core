61b0a1a4a1a4050cb97e160d82b46d6826045b8f
Gregory Collins <greg@gregorycollins.net>
2010-04-17 13:46:10 -0400

    Refactor routing stuff a little bit and improve docs

---
+++ src/Snap/Internal/Routing.hs
@@ -0,0 +1,168 @@
+module Snap.Internal.Routing where
+
+
+------------------------------------------------------------------------------
+import           Data.ByteString (ByteString)
+import           Data.ByteString.Internal (c2w)
+import qualified Data.ByteString as B
+import           Data.Monoid
+import qualified Data.Map as Map
+
+------------------------------------------------------------------------------
+import           Snap.Internal.Http.Types
+import           Snap.Internal.Types
+import           Snap.Internal.Debug
+
+
+------------------------------------------------------------------------------
+{-|
+
+The internal data type you use to build a routing tree.  Matching is
+done unambiguously.
+
+'Capture' and 'Dir' routes can have a "fallback" route:
+
+  - For 'Capture', the fallback is routed when there is nothing to capture
+  - For 'Dir', the fallback is routed when we can't find a route in its map
+
+Fallback routes are stacked: i.e. for a route like:
+
+> Dir [("foo", Capture "bar" (Action bar) NoRoute)] baz
+
+visiting the URI foo/ will result in the "bar" capture being empty and
+triggering its fallback. It's NoRoute, so we go to the nearest parent
+fallback and try that, which is the baz action.
+
+-}
+data Route = Action (Snap ())                     -- wraps a 'Snap' action
+           | Capture ByteString Route Route       -- captures the dir in a param
+           | Dir (Map.Map ByteString Route) Route -- match on a dir
+           | NoRoute
+
+
+instance Show Route where
+    show (Action _)       = "action"
+    show (Capture p r fb) = concat [ "capture ", show p, " (", show r, ")"
+                                   , " (fallback: ", show fb, ")" ]
+    show (Dir rm fb)      = concat [ "dir ", show rm
+                                   , " (fallback: ", show fb, ")" ]
+    show NoRoute          = "nothing"
+
+
+instance Monoid Route where
+    mempty = NoRoute
+
+    -- Unions two routes, favoring the right-hand side
+    mappend NoRoute r = r
+
+    mappend l@(Action _) r = case r of
+      (Action _)        -> r
+      (Capture p r' fb) -> Capture p r' (mappend fb l)
+      (Dir _ _)         -> mappend (Dir Map.empty l) r
+      NoRoute           -> l
+
+    mappend (Capture p r' fb) r = Capture p (mappend r' r) fb
+
+    mappend l@(Dir rm fb) r = case r of
+      (Action _)      -> Dir rm (mappend fb r)
+      (Capture _ _ _) -> Dir rm (mappend fb r)
+      (Dir rm' fb')   -> Dir (Map.unionWith mappend rm rm') (mappend fb fb')
+      NoRoute         -> l
+
+
+------------------------------------------------------------------------------
+-- | A web handler which, given a mapping from URL entry points to web
+-- handlers, efficiently routes requests to the correct handler.
+--
+-- The URL entry points are given as relative paths, for example:
+--
+-- > route [ ("foo/bar/quux", fooBarQuux) ]
+--
+-- If the URI of the incoming request is
+--
+-- > /foo/bar/quux
+--
+-- or
+--
+-- > /foo/bar/quux/...anything...
+--
+-- then the request will be routed to \"@fooBarQuux@\", with 'rqContextPath'
+-- set to \"@\/foo\/bar\/quux\/@\" and 'rqPathInfo' set to
+-- \"@...anything...@\".
+--
+-- @FIXME@\/@TODO@: we need a version with and without the context path setting
+-- behaviour; if the route is \"@article\/:id\/print@\", we probably want the
+-- contextPath to be \"@\/article@\" instead of \"@\/article\/:id\/print@\".
+--
+-- A path component within an URL entry point beginning with a colon (\"@:@\")
+-- is treated as a /variable capture/; the corresponding path component within
+-- the request URI will be entered into the 'rqParams' parameters mapping with
+-- the given name. For instance, if the routes were:
+--
+-- > route [ ("foo/:bar/baz", fooBazHandler) ]
+--
+-- Then a request for \"@\/foo\/saskatchewan\/baz@\" would be routed to
+-- @fooBazHandler@ with a mapping for:
+--
+-- > "bar" => "sasketchewan"
+--
+-- in its parameters table.
+--
+-- Longer paths are matched first, and specific routes are matched before
+-- captures. That is, if given routes:
+--
+-- > [ ("a", h1), ("a/b", h2), ("a/:x", h3) ]
+--
+-- a request for \"@\/a\/b@\" will go to @h2@, \"@\/a\/s@\" for any /s/ will go
+-- to @h3@, and \"@\/a@\" will go to @h1@.
+--
+-- The following example matches \"@\/article@\" to an article index,
+-- \"@\/login@\" to a login, and \"@\/article\/...@\" to an article renderer.
+--
+-- > route [ ("article",     renderIndex)
+-- >       , ("article/:id", renderArticle)
+-- >       , ("login",       method POST doLogin) ]
+--
+route :: [(ByteString, Snap ())] -> Snap ()
+route rts = do
+  -- FIXME How do we make this debug only print once?
+  debug $ "Types.route: " ++ show rts'
+  route' rts' []
+  where
+    rts' = mconcat (map pRoute rts)
+
+
+------------------------------------------------------------------------------
+pRoute :: (ByteString, Snap ()) -> Route
+pRoute (r, a) = foldr f (Action a) hier
+  where
+    hier   = filter (not . B.null) $ B.splitWith (== (c2w '/')) r
+    f s rt = if B.head s == c2w ':'
+        then Capture (B.tail s) rt NoRoute
+        else Dir (Map.fromList [(s, rt)]) NoRoute
+
+
+------------------------------------------------------------------------------
+route' :: Route -> [Route] -> Snap ()
+route' (Action action) _ = action
+
+route' (Capture param rt fb) fbs = do
+    cwd <- getRequest >>= return . B.takeWhile (/= (c2w '/')) . rqPathInfo
+    if B.null cwd
+      then route' fb fbs
+      else do modifyRequest $ updateContextPath (B.length cwd) . (f cwd)
+              route' rt (fb:fbs)
+  where
+    f v req = req { rqParams = Map.insertWith (++) param [v] (rqParams req) }
+
+route' (Dir rtm fb) fbs = do
+    cwd <- getRequest >>= return . B.takeWhile (/= (c2w '/')) . rqPathInfo
+    case Map.lookup cwd rtm of
+      Just rt -> do
+          modifyRequest $ updateContextPath (B.length cwd)
+          route' rt (fb:fbs)
+      Nothing -> route' fb fbs
+
+route' NoRoute       [] = pass
+route' NoRoute (fb:fbs) = route' fb fbs
+
---
+++ src/Snap/Internal/Types.hs
@@ -0,0 +1,359 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Snap.Internal.Types where
+
+------------------------------------------------------------------------------
+import           Control.Applicative
+import           Control.Exception
+import           Control.Monad.State.Strict
+import           Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Iteratee as Iter
+import           Data.Maybe
+import           Data.Typeable
+
+------------------------------------------------------------------------------
+import           Snap.Iteratee ( Iteratee
+                               , run
+                               , (>.)
+                               , fromWrap
+                               , stream2stream
+                               , enumBS
+                               , enumLBS )
+
+import           Snap.Internal.Http.Types
+
+
+------------------------------------------------------------------------------
+-- The Snap Monad
+------------------------------------------------------------------------------
+
+{-|
+
+'Snap' is the 'Monad' that user web handlers run in. 'Snap' gives you:
+
+1. stateful access to fetch or modify an HTTP 'Request'
+
+2. stateful access to fetch or modify an HTTP 'Response'
+
+3. failure \/ 'Alternative' \/ 'MonadPlus' semantics: a 'Snap' handler can
+   choose not to handle a given request, using 'empty' or its synonym 'pass',
+   and you can try alternative handlers with the '<|>' operator:
+
+   > a :: Snap String
+   > a = pass
+   >
+   > b :: Snap String
+   > b = return "foo"
+   >
+   > c :: Snap String
+   > c = a <|> b             -- try running a, if it fails then try b
+
+4. convenience functions ('writeBS', 'writeLBS', 'addToOutput') for writing
+output to the 'Response':
+
+   > a :: (forall a . Enumerator a) -> Snap ()
+   > a someEnumerator = do
+   >     writeBS "I'm a strict bytestring"
+   >     writeLBS "I'm a lazy bytestring"
+   >     addToOutput someEnumerator
+
+5. early termination: if you call 'finishWith':
+
+   > a :: Snap ()
+   > a = do
+   >   modifyResponse $ setResponseStatus 500
+   >   writeBS "500 error"
+   >   r <- getResponse
+   >   finishWith r
+
+   then any subsequent processing will be skipped and supplied 'Response' value
+   will be returned from 'runSnap' as-is.
+
+6. access to the 'IO' monad through a 'MonadIO' instance:
+
+   > a :: Snap ()
+   > a = liftIO fireTheMissiles
+-}
+
+newtype Snap a = Snap {
+      unSnap :: StateT SnapState (Iteratee IO) (Maybe (Either Response a))
+}
+
+
+data SnapState = SnapState
+    { _snapRequest  :: Request
+    , _snapResponse :: Response }
+
+
+instance Monad Snap where
+    (Snap m) >>= f =
+        Snap $ do
+            eth <- m
+            maybe (return Nothing)
+                  (either (return . Just . Left)
+                          (unSnap . f))
+                  eth
+
+    return = Snap . return . Just . Right
+    fail   = const $ Snap $ return Nothing
+
+instance MonadIO Snap where
+    liftIO m = Snap $ liftM (Just . Right) $ liftIO m
+
+
+instance MonadPlus Snap where
+    mzero = Snap $ return Nothing
+
+    a `mplus` b =
+        Snap $ do
+            mb <- unSnap a
+            if isJust mb then return mb else unSnap b
+
+
+instance Functor Snap where
+    fmap = liftM
+
+
+instance Applicative Snap where
+    pure  = return
+    (<*>) = ap
+
+    
+instance Alternative Snap where
+    empty = mzero
+    (<|>) = mplus
+
+
+------------------------------------------------------------------------------
+
+-- | Sends the request body through an iteratee (data consumer) and
+-- returns the result.
+runRequestBody :: Iteratee IO a -> Snap a
+runRequestBody iter = do
+    req  <- getRequest
+    let enum = rqBody req
+
+    -- make sure the iteratee consumes all of the output
+    let iter' = iter >>= (\a -> Iter.skipToEof >> return a)
+
+    -- run the iteratee
+    result <- liftIO $ enum iter' >>= run
+
+    -- stuff a new dummy enumerator into the request, so you can only try to
+    -- read the request body from the socket once
+    modifyRequest $ \r -> r { rqBody = enumBS "" }
+
+    return result
+
+
+-- | Returns the request body as a bytestring.
+getRequestBody :: Snap L.ByteString
+getRequestBody = liftM fromWrap $ runRequestBody stream2stream
+{-# INLINE getRequestBody #-}
+
+
+-- | Detaches the request body's 'Enumerator' from the 'Request' and
+-- returns it. You would want to use this if you needed to send the
+-- HTTP request body (transformed or otherwise) through to the output
+-- in O(1) space. (Examples: transcoding, \"echo\", etc)
+-- 
+-- Normally Snap is careful to ensure that the request body is fully
+-- consumed after your web handler runs; this function is marked
+-- \"unsafe\" because it breaks this guarantee and leaves the
+-- responsibility up to you. If you don't fully consume the
+-- 'Enumerator' you get here, the next HTTP request in the pipeline
+-- (if any) will misparse. Be careful with exception handlers.
+unsafeDetachRequestBody :: Snap (Enumerator a)
+unsafeDetachRequestBody = do
+    req <- getRequest
+    let enum = rqBody req
+    putRequest $ req {rqBody = enumBS ""}
+    return enum
+
+-- | Short-circuits a 'Snap' monad action early, storing the given
+-- 'Response' value in its state.
+finishWith :: Response -> Snap ()
+finishWith = Snap . return . Just . Left
+{-# INLINE finishWith #-}
+
+-- | Fails out of a 'Snap' monad action.  This is used to indicate
+-- that you choose not to handle the given request within the given
+-- handler.
+pass :: Snap a
+pass = empty
+
+-- | Runs a 'Snap' monad action only if the request's HTTP method matches
+-- the given method.
+method :: Method -> Snap a -> Snap a
+method m action = do
+    req <- getRequest
+    unless (rqMethod req == m) pass
+    action
+{-# INLINE method #-}
+
+
+-- Appends n bytes of the path info to the context path with a
+-- trailing slash.
+updateContextPath :: Int -> Request -> Request
+updateContextPath n req | n > 0     = req { rqContextPath = ctx
+                                          , rqPathInfo    = pinfo }
+                        | otherwise = req
+  where
+    ctx'  = B.take n (rqPathInfo req)
+    ctx   = B.concat [rqContextPath req, ctx', "/"]
+    pinfo = B.drop (n+1) (rqPathInfo req)
+
+-- Runs a 'Snap' monad action only if the 'rqPathInfo' matches the given
+-- predicate.
+pathWith :: (ByteString -> ByteString -> Bool)
+         -> ByteString
+         -> Snap a
+         -> Snap a
+pathWith c p action = do
+    req <- getRequest
+    unless (c p (rqPathInfo req)) pass
+    localRequest (updateContextPath $ B.length p) action
+
+
+-- | Runs a 'Snap' monad action only when the 'rqPathInfo' of the request
+-- starts with the given path. For example,
+--
+-- > dir "foo" handler
+--
+-- Will fail if 'rqPathInfo' is not \"@\/foo@\" or \"@\/foo\/...@\", and will
+-- add @\"foo\/\"@ to the handler's local 'rqContextPath'.
+dir :: ByteString  -- ^ path component to match
+    -> Snap a      -- ^ handler to run
+    -> Snap a
+dir = pathWith f
+  where
+    f dr pinfo = dr == x
+      where
+        (x,_) = B.break (=='/') pinfo
+{-# INLINE dir #-}
+
+
+-- | Runs a 'Snap' monad action only for requests where 'rqPathInfo' is exactly
+-- equal to the given string. If the path matches, locally sets 'rqContextPath'
+-- to the old value of 'rqPathInfo', sets 'rqPathInfo'=\"\", and runs the given
+-- handler.
+path :: ByteString  -- ^ path to match against
+     -> Snap a      -- ^ handler to run
+     -> Snap a
+path = pathWith (==)
+{-# INLINE path #-}
+
+
+-- | Runs a 'Snap' monad action only when 'rqPathInfo' is empty.
+top :: Snap a -> Snap a
+top = path ""
+{-# INLINE top #-}
+
+
+-- | Local Snap version of 'get'.
+sget :: Snap SnapState
+sget = Snap $ liftM (Just . Right) get
+{-# INLINE sget #-}
+
+-- | Local Snap monad version of 'modify'.
+smodify :: (SnapState -> SnapState) -> Snap ()
+smodify f = Snap $ modify f >> return (Just $ Right ())
+{-# INLINE smodify #-}
+
+-- | Grabs the 'Request' object out of the 'Snap' monad.
+getRequest :: Snap Request
+getRequest = liftM _snapRequest sget
+{-# INLINE getRequest #-}
+
+-- | Grabs the 'Response' object out of the 'Snap' monad.
+getResponse :: Snap Response
+getResponse = liftM _snapResponse sget
+{-# INLINE getResponse #-}
+
+-- | Puts a new 'Response' object into the 'Snap' monad.
+putResponse :: Response -> Snap ()
+putResponse r = smodify $ \ss -> ss { _snapResponse = r }
+{-# INLINE putResponse #-}
+
+-- | Puts a new 'Request' object into the 'Snap' monad.
+putRequest :: Request -> Snap ()
+putRequest r = smodify $ \ss -> ss { _snapRequest = r }
+{-# INLINE putRequest #-}
+
+-- | Modifies the 'Request' object stored in a 'Snap' monad.
+modifyRequest :: (Request -> Request) -> Snap ()
+modifyRequest f = smodify $ \ss -> ss { _snapRequest = f $ _snapRequest ss }
+{-# INLINE modifyRequest #-}
+
+-- | Modifes the 'Response' object stored in a 'Snap' monad.
+modifyResponse :: (Response -> Response) -> Snap () 
+modifyResponse f = smodify $ \ss -> ss { _snapResponse = f $ _snapResponse ss }
+{-# INLINE modifyResponse #-}
+
+
+-- | Adds the output from the given enumerator to the 'Response'
+-- stored in the 'Snap' monad state.
+addToOutput :: (forall a . Enumerator a)   -- ^ output to add
+            -> Snap ()
+addToOutput enum = modifyResponse $ modifyResponseBody (>. enum)
+
+
+-- | Adds the given strict 'ByteString' to the body of the 'Response'
+-- stored in the 'Snap' monad state.
+writeBS :: ByteString -> Snap ()
+writeBS s = addToOutput $ enumBS s
+
+
+-- | Adds the given lazy 'L.ByteString' to the body of the 'Response'
+-- stored in the 'Snap' monad state.
+writeLBS :: L.ByteString -> Snap ()
+writeLBS s = addToOutput $ enumLBS s
+
+
+-- | Runs a 'Snap' action with a locally-modified 'Request' state
+-- object. The 'Request' object in the Snap monad state after the call
+-- to localRequest will be unchanged.
+localRequest :: (Request -> Request) -> Snap a -> Snap a
+localRequest f m = do
+    req <- getRequest
+    modifyRequest f
+    result <- m
+    putRequest req
+    return result
+{-# INLINE localRequest #-}
+
+
+-- | This exception is thrown if the handler you supply to 'runSnap' fails.
+data NoHandlerException = NoHandlerException
+   deriving (Eq, Typeable)
+
+instance Show NoHandlerException where
+    show NoHandlerException = "No handler for request"
+
+instance Exception NoHandlerException
+
+
+-- | Runs a 'Snap' monad action in the 'Iteratee IO' monad.
+runSnap :: Snap a -> Request -> Iteratee IO (Request,Response)
+runSnap (Snap m) req = do
+    (r, ss') <- runStateT m ss
+
+    e <- maybe (liftIO $ throwIO NoHandlerException)
+               return
+               r
+
+    -- is this a case of early termination?
+    let resp = case e of 
+                 Left x  -> x
+                 Right _ -> _snapResponse ss'
+
+    return (_snapRequest ss', resp)
+
+  where
+    ss = SnapState req emptyResponse
+{-# INLINE runSnap #-}
