54fa1d1f4c868c9b041cc2f5e2ace7f4675b0fe3
Gregory Collins <greg@gregorycollins.net>
2010-04-19 00:19:05 -0400

    Switch to Droid Serif/Gill Sans for haddock docs to match site

--- haddock.sh
+++ haddock.sh
@@ -8,6 +8,6 @@ HADDOCK_OPTS='--html-location=http://hackage.haskell.org/packages/archive/$pkg/l
 
 cabal haddock $HADDOCK_OPTS --hyperlink-source $@
 
-
+cp -r extra/fonts dist/doc/html/snap-core/
 cp extra/logo.gif dist/doc/html/snap-core/haskell_icon.gif
 cp extra/hscolour.css dist/doc/html/snap-core/src/
