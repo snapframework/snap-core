76ee6887827eb34e7654d2a08742e2fa7b94ea57
Gregory Collins <greg@gregorycollins.net>
2010-05-02 01:26:33 -0400

    Make the debug stuff a configure flag

--- src/Snap/Internal/Debug.hs
+++ src/Snap/Internal/Debug.hs
@@ -1,15 +1,19 @@
 -- | An internal Snap module for (optionally) printing debugging
--- messages. Normally 'debug' does nothing, but you can hand-edit this file to
--- cause debug messages to be sent to @stdout@.
+-- messages. Normally 'debug' does nothing, but you can pass \"-fdebug\" to
+-- @cabal install@ to build a @snap-core@ which debugs to stderr.
 --
 -- /N.B./ this is an internal interface, please don't write external code that
 -- depends on it.
+
+{-# LANGUAGE CPP #-}
 
 module Snap.Internal.Debug where
 
 import           Control.Monad.Trans
 
-{-
+#ifdef DEBUG
+
+------------------------------------------------------------------------------
 import           Control.Concurrent
 import           Foreign.C.Error
 import           System.IO
@@ -21,19 +25,24 @@ _debugMVar = unsafePerformIO $ newMVar ()
 
 debug :: (MonadIO m) => String -> m ()
 debug s = liftIO $ withMVar _debugMVar $ \_ -> hPutStrLn stderr s >> hFlush stderr
+{-# INLINE debug #-}
 
 debugErrno :: (MonadIO m) => String -> m ()
 debugErrno loc = liftIO $ do
     err <- getErrno
     let ex = errnoToIOError loc err Nothing Nothing
     debug $ show ex
--}
+------------------------------------------------------------------------------
 
--- {-
+#else
+
+------------------------------------------------------------------------------
 debug :: (MonadIO m) => String -> m ()
 debug _ = return ()
+{-# INLINE debug #-}
 
 debugErrno :: (MonadIO m) => String -> m ()
 debugErrno _ = return ()
--- -}
-{-# INLINE debug #-}
+------------------------------------------------------------------------------
+
+#endif
