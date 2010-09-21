dc4fec02812b88810791de9f023e2a8ace19c559
Gregory Collins <greg@gregorycollins.net>
2010-09-21 12:48:21 -0400

    Major changes to debug mechanism
    What we used to do to enable debug logging is this: give the "-f debug" flag to
    cabal when installing snap-core, which used the C preprocessor to select a
    compile-time implementation for the "debug" function.
    
    What we're doing now instead is using "unsafePerformIO" to test the "DEBUG"
    environment variable the first time you call the debug function; subsequent
    calls to "debug" will re-use the fully-evaluated thunk, so you shouldn't pay a
    performance penalty if debug logging is turned off. We do pay a slight penalty
    because we can no longer inline calls to "debug", so debug overhead when
    debugging is off goes from a no-op to a call into a function pointer;
    benchmarks say the price to pay is slight and the productivity gains are
    enormous: set "DEBUG=1" when you invoke your server to get debugging output
    without recompiling the entire snap library stack.

--- src/Snap/Internal/Debug.hs
+++ src/Snap/Internal/Debug.hs
@@ -1,77 +1,112 @@
 -- | An internal Snap module for (optionally) printing debugging
--- messages. Normally 'debug' does nothing, but you can pass \"-fdebug\" to
--- @cabal install@ to build a @snap-core@ which debugs to stderr.
+-- messages. Normally 'debug' does nothing, but if you set @DEBUG=1@ in the
+-- environment you'll get debugging messages. We use 'unsafePerformIO' to make
+-- sure that the call to 'getEnv' is only made once.
 --
 -- /N.B./ this is an internal interface, please don't write external code that
 -- depends on it.
 
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
 
 module Snap.Internal.Debug where
 
-import           Control.Monad.Trans
-
-#ifdef DEBUG_TEST
-import           Control.DeepSeq
-
-debug :: (MonadIO m) => String -> m ()
-debug !s = let !s' = rnf s in return $! s' `deepseq` ()
-{-# INLINE debug #-}
-
-debugErrno :: (MonadIO m) => String -> m ()
-debugErrno !s = let !s' = rnf s in return $! s' `deepseq` ()
-
-#elif defined(DEBUG)
-
 ------------------------------------------------------------------------------
 import           Control.Concurrent
+import           Control.DeepSeq
+import           Control.Exception
+import           Control.Monad.Trans
+import           Data.Char
 import           Data.List
 import           Data.Maybe
 import           Foreign.C.Error
+import           System.Environment
 import           System.IO
 import           System.IO.Unsafe
 import           Text.Printf
 ------------------------------------------------------------------------------
 
 
+
+
+{-# NOINLINE debug #-}
+debug :: forall m . (MonadIO m => String -> m ())
+debug = let !x = unsafePerformIO $! do
+            !e <- try $ getEnv "DEBUG"
+            
+            !f <- either (\(_::SomeException) -> return debugIgnore)
+                         (\x -> if x == "1" || x == "on"
+                                  then return debugOn
+                                  else if x == "testsuite"
+                                         then return debugSeq
+                                         else return debugIgnore)
+                         (fmap (map toLower) e)
+            return $! f
+        in x
+
+
+{-# NOINLINE debugErrno #-}
+debugErrno :: forall m . (MonadIO m => String -> m ())
+debugErrno = let !x = unsafePerformIO $ do
+                 e <- try $ getEnv "DEBUG"
+                 
+                 !f <- either (\(_::SomeException) -> return debugErrnoIgnore)
+                              (\x -> if x == "1" || x == "on"
+                                       then return debugErrnoOn
+                                       else if x == "testsuite"
+                                              then return debugErrnoSeq
+                                              else return debugErrnoIgnore)
+                              (fmap (map toLower) e)
+                 return $! f
+             in x
+
+
+------------------------------------------------------------------------------
+debugSeq :: (MonadIO m) => String -> m ()
+debugSeq !s = let !s' = rnf s in return $! s' `deepseq` ()
+{-# NOINLINE debugSeq #-}
+
+debugErrnoSeq :: (MonadIO m) => String -> m ()
+debugErrnoSeq !s = let !s' = rnf s in return $! s' `deepseq` ()
+{-# NOINLINE debugErrnoSeq #-}
+
 ------------------------------------------------------------------------------
 _debugMVar :: MVar ()
 _debugMVar = unsafePerformIO $ newMVar ()
 {-# NOINLINE _debugMVar #-}
 
+
 ------------------------------------------------------------------------------
-debug :: (MonadIO m) => String -> m ()
-debug s = liftIO $ withMVar _debugMVar $ \_ -> do
-              tid <- myThreadId
-              hPutStrLn stderr $ s' tid
-              hFlush stderr
+debugOn :: (MonadIO m) => String -> m ()
+debugOn s = liftIO $ withMVar _debugMVar $ \_ -> do
+                tid <- myThreadId
+                hPutStrLn stderr $ s' tid
+                hFlush stderr
   where
     chop x = let y = fromMaybe x $ stripPrefix "ThreadId " x
              in printf "%8s" y
 
     s' t   = "[" ++ chop (show t) ++ "] " ++ s
 
-{-# INLINE debug #-}
+{-# NOINLINE debugOn #-}
 
 
 ------------------------------------------------------------------------------
-debugErrno :: (MonadIO m) => String -> m ()
-debugErrno loc = liftIO $ do
+debugErrnoOn :: (MonadIO m) => String -> m ()
+debugErrnoOn loc = liftIO $ do
     err <- getErrno
     let ex = errnoToIOError loc err Nothing Nothing
     debug $ show ex
 ------------------------------------------------------------------------------
 
-#else
 
 ------------------------------------------------------------------------------
-debug :: (MonadIO m) => String -> m ()
-debug _ = return ()
-{-# INLINE debug #-}
+debugIgnore :: (MonadIO m) => String -> m ()
+debugIgnore _ = return ()
+{-# NOINLINE debugIgnore #-}
 
-debugErrno :: (MonadIO m) => String -> m ()
-debugErrno _ = return ()
+debugErrnoIgnore :: (MonadIO m) => String -> m ()
+debugErrnoIgnore _ = return ()
 ------------------------------------------------------------------------------
-
-#endif
