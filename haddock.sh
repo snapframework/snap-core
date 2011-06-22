92458a2da5a11aee1d55d0dc7f0efb9843a6a2bf
Gregory Collins <greg@gregorycollins.net>
2011-06-22 09:53:56 -0400

    Remove extra css

--- haddock.sh
+++ haddock.sh
@@ -4,7 +4,7 @@ set -x
 
 rm -Rf dist/doc
 
-HADDOCK_OPTS='--html-location=http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html --css=extra/haddock.css'
+HADDOCK_OPTS='--html-location=http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html'
 
 cabal haddock $HADDOCK_OPTS --hyperlink-source $@
 
