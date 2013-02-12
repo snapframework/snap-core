2677602594267927a7c11381a22a0401ee0605f1
Gregory Collins <greg@gregorycollins.net>
2013-02-12 13:12:18 +0100

    Remove deprecated Snap.Types module.

--- src/Snap/Types.hs
+++
@@ -1,6 +0,0 @@
--- | As of Snap 0.6, the "Snap.Types" module is deprecated: please use
--- "Snap.Core" instead.
-module Snap.Types {-# DEPRECATED "As of 0.6, use Snap.Core instead" #-}
-  ( module Snap.Core ) where
-
-import Snap.Core
