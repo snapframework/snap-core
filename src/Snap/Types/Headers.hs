82575abb098a6da4d0d63264cbef8db297a8f495
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2014-06-05 23:53:14 +0200

    Document the 'Snap.Types.Headers' module.

--- src/Snap/Types/Headers.hs
+++ src/Snap/Types/Headers.hs
@@ -60,22 +60,54 @@ import           Prelude                     (Bool (..), Eq (..), Maybe (..), Sh
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
+-- | A key-value map that represents a collection of HTTP header fields. Keys
+-- are case-insensitive.
 newtype Headers = H { unH :: [(ByteString, ByteString)] }
   deriving (Show)
 
 
 ------------------------------------------------------------------------------
+-- | An empty collection of HTTP header fields.
+--
+-- Example:
+--
+-- @
+-- ghci> H.'empty'
+-- H {unH = []}
+-- @
 empty :: Headers
 empty = H []
 
 
 ------------------------------------------------------------------------------
+-- | Is a given collection of HTTP header fields empty?
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> H.'null' H.'empty'
+-- True
+-- ghci> H.'null' $ H.'fromList' [(\"Host\", \"localhost\")]
+-- False
+-- @
 null :: Headers -> Bool
 null = List.null . unH
 {-# INLINE null #-}
 
 
 ------------------------------------------------------------------------------
+-- | Does this collection of HTTP header fields contain a given field?
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> H.'member' \"host\" $ H.'fromList' [(\"Host\", \"localhost\")]
+-- True
+-- ghci> H.'member' \"Accept\" $ H.'fromList' [(\"Host\", \"localhost\")]
+-- False
+-- @
 member :: CI ByteString -> Headers -> Bool
 member k0 = f . unH
   where
@@ -85,19 +117,54 @@ member k0 = f . unH
 
 
 ------------------------------------------------------------------------------
+-- | Look up the value of a given HTTP header field.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> H.'lookup' \"host\" $ H.'fromList' [(\"Host\", \"localhost\")]
+-- Just \"localhost\"
+-- ghci> H.'lookup' \"Accept\" $ H.'fromList' [(\"Host\", \"localhost\")]
+-- Nothing
+-- @
 lookup :: CI ByteString -> Headers -> Maybe ByteString
 lookup k (H m) = List.lookup (CI.foldedCase k) m
 {-# INLINE lookup #-}
 
 
 ------------------------------------------------------------------------------
+-- | Look up the value of a given HTTP header field or return the provided
+-- default value when that header field is not present.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> let hdrs = H.'fromList' [(\"Host\", \"localhost\")]
+-- ghci> H.'lookupWithDefault' \"host\" \"127.0.0.1\" $ hdrs
+-- \"localhost\"
+-- ghci> H.'lookupWithDefault' \"Accept\" \"text\/plain\" $ hdrs
+-- \"text\/plain\"
+-- @
 lookupWithDefault :: ByteString -> CI ByteString -> Headers -> ByteString
 lookupWithDefault d k m = fromMaybe d $ lookup k m
 
 
 ------------------------------------------------------------------------------
--- | Insert a key-value into the headers map. If the key already exists in the
--- map, the values are catenated with ", ".
+-- | Insert a key-value pair into the headers map. If the key already exists in
+-- the map, the values are catenated with ", ".
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> let hdrs = H.'insert' \"Accept\" \"text\/plain\" $ H.'empty'
+-- ghci> hdrs
+-- H {unH = [(\"accept\",\"text\/plain\")]}
+-- ghci> H.'insert' \"Accept\" \"text\/html\" $ hdrs
+-- H {unH = [(\"accept\",\"text\/plain,text\/html\")]}
+-- @
 insert :: CI ByteString -> ByteString -> Headers -> Headers
 insert k0 v (H m) = H $! go id m
   where
@@ -109,14 +176,40 @@ insert k0 v (H m) = H $! go id m
 
 
 ------------------------------------------------------------------------------
--- | Insert a key-value into the headers map, without checking whether the
+-- | Insert a key-value pair into the headers map, without checking whether the
 -- header already exists. The key /must/ be already case-folded, or none of the
 -- lookups will work!
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> let hdrs = H.'unsafeInsert' \"accept\" \"text\/plain\" $ H.'empty'
+-- ghci> hdrs
+-- H {unH = [(\"accept\",\"text\/plain\")]}
+-- ghci> let hdrs' = H.'unsafeInsert' \"accept\" \"text\/html\" $ hdrs
+-- ghci> hdrs'
+-- H {unH = [(\"accept\",\"text\/html\"), (\"accept\",\"text\/plain\")]}
+-- ghci> H.'lookup' \"accept\" hdrs'
+-- Just \"text\/html\"
+-- @
 unsafeInsert :: ByteString -> ByteString -> Headers -> Headers
 unsafeInsert k v (H hdrs) = H ((k,v):hdrs)
 
 
 ------------------------------------------------------------------------------
+-- | Set the value of a HTTP header field to a given value, replacing the old
+-- value.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> H.'set' \"accept\" \"text\/plain\" $ H.'empty'
+-- H {unH = [(\"accept\",\"text\/plain\")]}
+-- ghci> H.'set' \"accept\" \"text\/html\" $ H.'fromList' [(\"Accept\", \"text\/plain\")]
+-- H {unH = [(\"accept\",\"text\/html\")]}
+-- @
 set :: CI ByteString -> ByteString -> Headers -> Headers
 set k0 v (H m) = H $ go m
   where
@@ -128,6 +221,16 @@ set k0 v (H m) = H $ go m
 
 
 ------------------------------------------------------------------------------
+-- | Delete all key-value pairs associated with the given key from the headers
+-- map.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> H.'delete' \"accept\" $ H.'fromList' [(\"Accept\", \"text\/plain\")]
+-- H {unH = []}
+-- @
 delete :: CI ByteString -> Headers -> Headers
 delete k (H m) = H $ List.filter ((k' /=) . fst) m
   where
@@ -135,6 +238,18 @@ delete k (H m) = H $ List.filter ((k' /=) . fst) m
 
 
 ------------------------------------------------------------------------------
+-- | Strict left fold over all key-value pairs in the headers map.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> import "Data.Monoid"
+-- ghci> let hdrs = H.'fromList' [(\"Accept\", \"text\/plain\"), (\"Accept\", \"text\/html\")]
+-- ghci> let f (cntr, acc) _ val = (cntr+1, val <> \";\" <> acc)
+-- ghci> H.'foldl'' f (0, \"\") hdrs
+-- (2,\"text\/html;text\/plain;\")
+-- @
 foldl' :: (a -> CI ByteString -> ByteString -> a)
        -> a
        -> Headers
@@ -145,6 +260,8 @@ foldl' f a (H m) = List.foldl' f' a m
 
 
 ------------------------------------------------------------------------------
+-- | Same as 'foldl'', but the key parameter is of type 'ByteString' instead of
+-- 'CI' 'ByteString'. The key is case-folded (lowercase).
 foldedFoldl' :: (a -> ByteString -> ByteString -> a)
              -> a
              -> Headers
@@ -155,6 +272,18 @@ foldedFoldl' f a (H m) = List.foldl' f' a m
 
 
 ------------------------------------------------------------------------------
+-- | Right fold over all key-value pairs in the headers map.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> import "Data.Monoid"
+-- ghci> let hdrs = H.'fromList' [(\"Accept\", \"text\/plain\"), (\"Accept\", \"text\/html\")]
+-- ghci> let f _ val (cntr, acc) = (cntr+1, val <> \";\" <> acc)
+-- ghci> H.'foldr' f (0, \"\") hdrs
+-- (2,\"text\/plain;text\/html;\")
+-- @
 foldr :: (CI ByteString -> ByteString -> a -> a)
       -> a
       -> Headers
@@ -165,6 +294,8 @@ foldr f a (H m) = List.foldr f' a m
 
 
 ------------------------------------------------------------------------------
+-- | Same as 'foldr', but the key parameter is of type 'ByteString' instead of
+-- 'CI' 'ByteString'. The key is case-folded (lowercase).
 foldedFoldr :: (ByteString -> ByteString -> a -> a)
             -> a
             -> Headers
@@ -173,21 +304,44 @@ foldedFoldr f a (H m) = List.foldr (uncurry f) a m
 
 
 ------------------------------------------------------------------------------
+-- | Convert a 'Headers' value to a list of key-value pairs.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> let l = [(\"Accept\", \"text\/plain\"), (\"Accept\", \"text\/html\")]
+-- ghci> H.'toList' . H.'fromList' $ l
+-- [(\"accept\",\"text\/plain\"),(\"accept\",\"text\/html\")]
+-- @
 toList :: Headers -> [(CI ByteString, ByteString)]
 toList = map (first CI.unsafeMk) . unH
 
 
 ------------------------------------------------------------------------------
+-- | Build a 'Headers' value from a list of key-value pairs.
+--
+-- Example:
+--
+-- @
+-- ghci> :set -XOverloadedStrings
+-- ghci> H.'fromList' [(\"Accept\", \"text\/plain\"), (\"Accept\", \"text\/html\")]
+-- H {unH = [(\"accept\",\"text\/plain\"),(\"accept\",\"text\/html\")]}
+-- @
 fromList :: [(CI ByteString, ByteString)] -> Headers
 fromList = H . map (first CI.foldedCase)
 
 
 ------------------------------------------------------------------------------
+-- | Like 'fromList', but the keys are assumed to be already case-folded (in
+-- lowercase).
 unsafeFromCaseFoldedList :: [(ByteString, ByteString)] -> Headers
 unsafeFromCaseFoldedList = H
 
 
 ------------------------------------------------------------------------------
+-- | Like 'toList', but does not convert the keys to 'CI' 'ByteString', so key
+-- comparisons will be case-sensitive.
 unsafeToCaseFoldedList :: Headers -> [(ByteString, ByteString)]
 unsafeToCaseFoldedList = unH
 
