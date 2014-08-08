046d0ed30935c499ef615d96d8dd71c186874c89
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2014-08-08 18:11:52 +0200

    Document the 'Snap.Util.FileServe' module.

--- src/Snap/Util/FileServe.hs
+++ src/Snap/Util/FileServe.hs
@@ -1,6 +1,32 @@
 -- | Contains web handlers to serve files from a directory.
+--
+-- Example usage:
+--
+-- @
+-- {-# LANGUAGE OverloadedStrings #-}
+--
+-- module Main where
+--
+-- import "Snap.Core"           (Snap, route)
+-- import "Snap.Http.Server"    (quickHttpServe)
+-- import "Snap.Util.FileServe"
+--
+-- site :: Snap ()
+-- site = 'Snap.Core.route'
+--   [ (\"\/files\",   'serveDirectory'                            \"static\")
+--   , (\"\/simple\",  'serveDirectoryWith' 'simpleDirectoryConfig'  \"static\")
+--   , (\"\/default\", 'serveDirectoryWith' 'defaultDirectoryConfig' \"static\")
+--   , (\"\/fancy\",   'serveDirectoryWith' 'fancyDirectoryConfig'   \"static\")
+--   , (\"\/paper\",   'serveFile'                                 \"static\/paper.pdf\")
+--   , (\"\/thesis\",  'serveFileAs'        \"application\/pdf\"      \"static\/thesis.pdf\")
+--   ]
+--
+-- main :: IO ()
+-- main = 'Snap.Http.Server.quickHttpServe' site
+-- @
 module Snap.Util.FileServe
-  ( getSafePath
+  ( -- * Helper functions
+    getSafePath
     -- * Configuration for directory serving
   , MimeMap
   , HandlerMap
