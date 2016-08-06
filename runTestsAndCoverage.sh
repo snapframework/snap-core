9f6e82899956aa06ef5050f10a9160069cc9f448
Doug Beardsley <mightybyte@gmail.com>
2016-08-06 13:50:44 -0400

    Update runTestsAndCoverage.sh

--- runTestsAndCoverage.sh
+++ runTestsAndCoverage.sh
@@ -2,33 +2,19 @@
 
 set -e
 
-if [ -z "$DEBUG" ]; then
-    export DEBUG="testsuite"
-fi
-
-SUITE=./dist/build/testsuite/testsuite
-
 export LC_ALL=C
 export LANG=C
 
 rm -f testsuite.tix
 
-if [ ! -f $SUITE ]; then
-    cat <<EOF
-Testsuite executable not found, please run:
-    cabal configure --enable-tests
-then
-    cabal build
-EOF
-    exit;
-fi
+# TODO How do we find the executable without knowing the version number in dist-newstyle?
+./dist-newstyle/build/snap-server-1.0.0.0/build/testsuite/testsuite -j4 -a1000 $*
 
-./dist/build/testsuite/testsuite -j4 -a1000 $*
-
-DIR=dist/hpc
+DIR="./dist-newstyle/hpc"
 
 rm -Rf $DIR
 mkdir -p $DIR
+mkdir -p out
 
 EXCLUDES='Main
 Snap.Core.Tests
@@ -55,7 +41,7 @@ for m in $EXCLUDES; do
     EXCL="$EXCL --exclude=$m"
 done
 
-hpc markup $EXCL --destdir=$DIR testsuite >/dev/null 2>&1
+hpc markup $EXCL --destdir=$DIR testsuite
 
 rm -f testsuite.tix
 
